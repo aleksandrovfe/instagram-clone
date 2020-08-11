2ae64b8dd4d4b736dabecdf26f9f6d5e168c58f8
aleksandrov <aaleksandrov@bears.software>
2020-08-11 09:34:05 +0300

    Updates

---
+++ precache-manifest.14bd39963af217036b1e3b4c9731514a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de70d8a2b8a642e14c7afe919c2251a",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "81b0b111c377ee238ffd",
+    "url": "/instagram-clone/static/css/main.e82b0e0f.chunk.css"
+  },
+  {
+    "revision": "cfb2d505f9d84c3518b9",
+    "url": "/instagram-clone/static/js/2.a4a80671.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone/static/js/2.a4a80671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b0b111c377ee238ffd",
+    "url": "/instagram-clone/static/js/main.810d760a.chunk.js"
+  },
+  {
+    "revision": "02a87c1a75cba5191192",
+    "url": "/instagram-clone/static/js/runtime-main.c334b797.js"
+  }
+]);
