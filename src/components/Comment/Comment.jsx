7b96733f75eb6d0f14f2d938a5fd68847347a604
Aleksandr Aleksandrov <47118920+aleksandrovfe@users.noreply.github.com>
2020-08-11 09:31:52 +0300

    rename (#1)
    Co-authored-by: aleksandrov <aaleksandrov@bears.software>

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -6,7 +6,7 @@ export const Comment = ({ comments }) => {
             {
                 comments.map(({id, comment}) => (
                     <p className="comments__text" key={id}>
-                        <strong>{comment.userName}</strong> {comment.text}
+                        <b>{comment.userName}</b> {comment.text}
                     </p>
                 ))
             }
